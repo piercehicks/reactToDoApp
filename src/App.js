a1816f9cd517ffbcb78a91759fbbe66ca42eb350
Pierce <phicks21@gmail.com>
2018-09-29 08:48:43 -0400

    react todo state and prop 1

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props) {
+    super(prop);
+  this.state = {
+    todos:[
+      { description: 'Buy Birthday gift', isCompleted:true },
+      { description: 'Take out trash', isCompleted: false },
+      { description: "Meet up with Eddy", isCompleted: false }
+    ]
+  };
+  }
   render() {
     return (
       <div className="App">
       <ul>
-         <ToDo />
-         <ToDo />
-       </ul> 
+      { this.state.todos.map( (todo, index) =>
+       <ToDo key={ index } description={todo.description} isCompleted={todo.isCompleted}/>
+     )}
+       </ul>
       </div>
     );
   }
